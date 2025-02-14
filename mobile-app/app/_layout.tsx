1474fecfe38a14df9ec0397e8959552b73e9bbc9
Bavwoshiya <bavwoshiya@gmail.com>
2025-02-14 12:12:26 +0100

    react native setup

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,15 +1,38 @@
-import { Stack } from "expo-router";
-import { View } from "react-native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          fullScreenGestureEnabled: true,
-        }}
-      />
-    </View>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
--- mobile-app/constants/icons.ts
+++
@@ -1,4 +0,0 @@
-import Logo from "@/assets/images/LyricsFlipLogo.png";
-import bg from "@/assets/images/hero-background.png";
-import menu from "@/assets/images/menu.png";
-export default { Logo, bg, menu };
